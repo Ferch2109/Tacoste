5302cbaa64457dfdf85ae2463a42e7aee629e6f4
MarkHG <markhg@ciencias.unam.mx>
2017-12-27 19:21:56 -0600

    Borrar.sql

---
+++ sql/borrar.sql
@@ -0,0 +1,56 @@
+DROP TABLE comensal;
+DROP TABLE datos_comensal;
+DROP TABLE pedido;
+DROP TABLE mesa;
+DROP TABLE producto;
+DROP TABLE contener;
+DROP TABLE categoria_producto;
+DROP TABLE ingrediente;
+DROP TABLE participar;
+DROP TABLE consumir;
+DROP TABLE etiqueta_producto;
+DROP TABLE salsa;
+DROP TABLE presentacion_salsa;
+DROP TABLE emparejar;
+DROP TABLE sucursal;
+DROP TABLE gerencia_sucursal;
+DROP TABLE informacion_sucursal;
+DROP TABLE telefono_sucursal;
+DROP TABLE empleado;
+DROP TABLE datos_empleado;
+DROP TABLE correo_empleado;
+DROP TABLE telefono_empleado;
+DROP TABLE supervisar;
+DROP TABLE licencia;
+DROP TABLE licencia_repartidor;
+DROP TABLE datos_licencia;
+DROP TABLE repartidor;
+DROP TABLE proveedor;
+DROP TABLE identificacion_proveedor;
+DROP TABLE datos_proveedor;
+DROP TABLE suministro;
+DROP TABLE nombre_suministro;
+DROP TABLE datos suministro;
+DROP TABLE inventario;
+DROP TABLE suministrar;
+DROP TABLE generar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
