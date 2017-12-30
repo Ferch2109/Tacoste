99c7cd7118c34fad93c5b9d133cd2c5b7b3915ba
Ángel Gladín <angelgladin@gmail.com>
2017-12-29 20:31:52 -0600

    Todo el boilerplate de la aplicación

---
+++ app/src/fbd/controller/RootController.java
@@ -0,0 +1,38 @@
+package fbd.controller;
+
+import javafx.fxml.FXML;
+
+public class RootController {
+
+    @FXML public void launchTaqueriasSection() {
+
+    }
+
+    @FXML public void launchEmployeesSection() {
+    }
+
+    @FXML public void launchClientsSection() {
+    }
+
+    @FXML public void launchProductsSection() {
+    }
+
+    @FXML public void launchProvidersSection() {
+    }
+
+    @FXML public void launchGenerateReport() {
+    }
+
+    @FXML public void testConnection() {
+    }
+
+    @FXML public void addProductFromMenu() {
+    }
+
+    @FXML public void removeProductFromOrderList() {
+    }
+
+    @FXML public void orderProductsFromOrderList() {
+    }
+
+}
