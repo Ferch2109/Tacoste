6ba0ac5c208fd0bc7c0781bf8a91a51501360031
Ángel Gladín <angelgladin@gmail.com>
2018-01-05 02:31:21 -0600

    Se me olvidó agregaron unos putos archivos

---
+++ app/src/fbd/util/TableViewUtil.java
@@ -0,0 +1,85 @@
+package fbd.util;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class TableViewUtil {
+
+    @SuppressWarnings("unchecked")
+    public static void addTableColumnsDynamically(TableView<ObservableList> tableView,
+                                                  String titleAlias,
+                                                  ObservableList<ObservableList> data) {
+        try {
+            Connection c;
+            c = DBUtil.getConnection();
+            //SQL FOR SELECTING ALL OF CUSTOMER
+            String SQL = "SELECT * FROM " + getTable(titleAlias);
+            //ResultSet
+            ResultSet rs = c.createStatement().executeQuery(SQL);
+
+            /**********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             **********************************/
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param ->
+                        new SimpleStringProperty(param.getValue().get(j).toString()));
+
+                tableView.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
+            }
+
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    String s = rs.getString(i);
+                    row.add(s != null ? s : "");
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tableView.setItems(data);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static String getTable(String titleAlias) {
+        //TODO completear el pendejo nombre de las putas tablas
+        switch (titleAlias) {
+            case "Taquerias":
+                return "informacion_sucursal";
+            case "Empleados":
+                return "";
+            case "Clientes":
+                return "";
+            case "Productos":
+                return "";
+            case "Provedores":
+                return "";
+            default:
+                return "";
+        }
+    }
+
+}
