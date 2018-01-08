65d730415069e5dc2c4c7c82f5b23bffcabf614b
Ferch2109 <fernandagch@ciencias.unam.mx>
2018-01-07 23:48:37 -0600

    Faltó incluir correos

--- sql/Empleados_consultas-apoyo.sql
+++ sql/Empleados_consultas-apoyo.sql
@@ -2,6 +2,8 @@ select * from empleado;
 
 select * from telefono_empleado;
 
+select * from correo_empleado;
+
 /*Info licencia*/
 select curp,tipo,vigencia,transporte from repartidor natural join datos_licencia;
 
@@ -30,7 +32,7 @@ order by nombre;
 select * from (select *
                 from (select * from (select curp,rfc,nombre||' '||paterno||' '||materno as nombre, sueldo from empleado) 
                                     natural join 
-                                    (select curp,colonia||', '||cp||' '||municipio||', '||estado as direccion from datos_empleado))
+                                    (select curp,colonia||', '||cp||' '||municipio||', '||estado as direccion,correo from datos_empleado natural join correo_empleado))
                       natural join (select curp,"1" as tel1,"2" as tel2,"3" as tel3 from (select * from (select * from telefono_empleado) natural join (SELECT curp, telefono_empleado, ROW_NUMBER() OVER (PARTITION BY curp ORDER BY curp) AS tel FROM telefono_empleado) )
                 pivot ( sum(telefono_empleado) for tel in (1,2,3) )))
                 natural join
