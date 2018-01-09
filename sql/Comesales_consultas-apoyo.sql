e023b33f974ea697e7315c0b2b1eda490fcf6695
Ferch2109 <fernandagch@ciencias.unam.mx>
2018-01-08 20:57:12 -0600

    Consultillas para obtener info sobre comensales

---
+++ sql/Comesales_consultas-apoyo.sql
@@ -0,0 +1,16 @@
+select * from comensal;
+
+select * from datos_comensal;
+
+--nombre
+select id_comensal,nombre||' '||paterno||' '||materno as nombre from comensal;
+
+--dir = "calle num, del ,col,cp municipio,estado"
+select id_comensal,calle||' '||numero||', '||colonia||', '||cp||' '||municipio||', '||estado as direccion,telefono,correo,puntos from datos_comensal order by id_comensal;
+
+
+/*Info comensal*/
+select * from (select id_comensal,nombre||' '||paterno||' '||materno as nombre from comensal) 
+              natural join 
+              (select id_comensal,calle||' '||numero||', '||colonia||', '||cp||' '||municipio||', '||estado as direccion,correo,telefono,puntos from datos_comensal)
+order by id_comensal;
