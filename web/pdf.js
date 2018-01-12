3757720c1ae48a1773ccc7168e5a7f2a319b7368
Ferch2109 <fernandagch@ciencias.unam.mx>
2018-01-12 08:54:19 -0600

    Descarga pdf

--- web/pdf.js
+++ web/pdf.js
@@ -3,8 +3,8 @@ window.onload = function(){
 				"<link rel='stylesheet' type='text/css' href='reporte.css'>"+
 				"<!--<<script type='conic-gradient.js'></script>--></head>"
 	var body = "<body>"+document.getElementsByTagName('body')[0].innerHTML+"</body>";
-
-	var printWindow = window.open('', '', 'height=400,width=800');
+	var source = '<html>'+head+body+'</html>';
+/*	var printWindow = window.open('', '', 'height=400,width=800');
 	
 	printWindow.document.write('<html>');
 	printWindow.document.write(head);
@@ -12,4 +12,15 @@ window.onload = function(){
 	printWindow.document.write('</html>');
 	printWindow.document.close();
 	printWindow.print();
+*/
+
+
+html2canvas(document.body,{onrendered: function(canvas){
+		var img = canvas.toDataURL("image/png");
+		var doc = new jsPDF();
+		var width = canvas.width;
+		var height = canvas.clientHeight;
+		doc.addImage(img, 'PNG', 10, 10);
+		doc.save('text.pdf');
+	}});
 }
