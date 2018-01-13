9292cfc4ceea2dc691e6eda8963a5fe3fe60e2f7
Fernando Fong <fernandofong@ciencias.unam.mx>
2018-01-12 20:50:49 -0600

    Pegué mis consultas que pude hacer.

--- sql/consultasT.sql
+++ sql/consultasT.sql
@@ -28,3 +28,30 @@ FROM empleado NATURAL JOIN (SELECT curp
                            MINUS
                            SELECT curp
                                         FROM repartidor);
+--Esta consulta nos muestra con qué prefieren pagar los clientes, para
+esto usamos
+--tablas temporales, para almacenar cuántas veces se pagó con qué
+metodo y después
+--únicamente sacar el máximo.
+CREATE TABLE cuenta_veces
+AS SELECT count(forma_pago) pago FROM pedido
+WHERE (lower(forma_pago) = 'efectivo') OR (lower(forma_pago) = 'tarjeta')
+GROUP BY forma_pago;
+
+SELECT max(pago) as preferida FROM cuenta_veces;
+
+--Que salsa es la que más participa en los productos que vende la taquería.
+CREATE TABLE cuenta_apariciones
+AS SELECT count(id_salsa) veces FROM emparejar
+GROUP BY id_salsa;
+
+SELECT * FROM salsa
+WHERE id_producto = (SELECT max(veces) FROM cuenta_apariciones);
+
+--Vamos a ver qué producto es el que piden más en cualquier sucursal.
+CREATE TABLE veces_productos
+AS SELECT count(id_producto) apariciones FROM contener
+GROUP BY id_producto;
+
+SELECT nombre FROM producto
+WHERE id_producto = (SELECT max(apariciones) FROM veces_productos);
